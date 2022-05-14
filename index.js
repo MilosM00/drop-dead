857ef5e502532c7fd235ed4265e505d466d1864c
Milos Milijasevic <milosrm20005@gmail.com>
2022-05-14 19:46:55 +0200

    js updated

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 "use strict";
 
 
+const scorePlayerOne = document.querySelector(`.score-player1`);
+const diceScorePlayerOne = document.querySelector(`.dice-score-player1`);
+const scorePlayerTwo = document.querySelector(`.score-player2`);
+const diceScorePlayerTwo = document.querySelector(`.dice-score-player2`);
+const diceOne = document.querySelector(`.dice-1`);
+const diceTwo = document.querySelector(`.dice-2`);
+const diceThree = document.querySelector(`.dice-3`);
+const diceFour = document.querySelector(`.dice-4`);
+const diceFive = document.querySelector(`.dice-5`);
+const diceSix = document.querySelector(`.dice-6`);
+const diceSeven = document.querySelector(`.dice-7`);
+const diceEight = document.querySelector(`.dice-8`);
+const diceNine = document.querySelector(`.dice-9`);
+const diceTen = document.querySelector(`.dice-10`);
+const rollDicePlayerOne = document.querySelector(`.roll-dice-player1`);
+const rollDicePlayerTwo = document.querySelector(`.roll-dice-player2`);
+const winnerIs = document.querySelector(`.winner`);
+const buttonNewGame = document.querySelector(`.new-game`);
+const buttonPlayAgain = document.querySelector(`.play-again`);
+
+
